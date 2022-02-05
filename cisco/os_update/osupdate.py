cb8feedb4f6cfd929cdb1bdb0f849a05afadeb5e
mmallikpy <mmallikpy@gmail.com>
2022-02-05 22:50:11 +0600

    con

--- cisco/os_update/osupdate.py
+++ cisco/os_update/osupdate.py
@@ -1,10 +1,1 @@
-"""
-This project goal
-1. Download os form cisco websites based on device model and last stable version.
-2. Create a TFTP/WEB/FTP server and store the os file in that folder.
-3. Upload the ios file on cisco device.
-4. Check the hash
-5. Take a backup before update.
-6. And some information will collect like Uptime, memory, os version, hostname, device serial, cdp, routing status, ect...
-7. Then update.
-"""
+
